081804eaf0091b3ca1444e51f49b438dd764e20f
Israa Mohamed <e.mohamed@shabek.com>
2025-04-15 19:50:37 +0200

    update meal model

--- src/main/kotlin/data/models/Meal.kt
+++ src/main/kotlin/data/models/Meal.kt
@@ -2,12 +2,13 @@ package data.models
 
 data class Meal (
     val name:String,
-    val id:Long,
+    val id:Int,
     val minutes:Int,
     val tags:List<String>,
     val description:String?,
     val nutrition:Nutrition,
     val steps:Steps,
     val ingredients:Ingredients,
-    val submitted:String
+    val submitted:String,
+    val contributorId:Int,
 )
