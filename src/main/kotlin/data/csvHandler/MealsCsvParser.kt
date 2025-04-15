deb8a52537abb91b534ec6aa78e955225b40e76b
Israa Mohamed <e.mohamed@shabek.com>
2025-04-15 18:57:18 +0200

    update parse one line method

--- src/main/kotlin/data/csvHandler/MealsCsvParser.kt
+++ src/main/kotlin/data/csvHandler/MealsCsvParser.kt
@@ -1,19 +1,36 @@
 package data.csvHandler
 
+import data.models.Ingredients
 import data.models.Meal
+import data.models.Nutrition
+import data.models.Steps
+import kotlin.math.min
 
 
 class MealsCsvParser() {
 
-
-    fun parseOnLineString(line: String): List<String> {
-        return line.removeSurrounding("\"").split(",(?![^\\[]*\\])".toRegex())
+    fun parseOnLine(line: String): Meal{
+        val mealInfo = line.removeSurrounding("\"").split(",(?![^\\[]*\\])".toRegex())
+        return Meal(
+            name = mealInfo[ColumnIndex.NAME],
+            id = mealInfo[ColumnIndex.ID].toLong(),
+            minutes = mealInfo[ColumnIndex.MINUTES].toInt(),
+            description = mealInfo[ColumnIndex.DESCRIPTION],
+            ingredients = Ingredients(
+                ingredients = mealInfo[ColumnIndex.INGREDIENTS].toListOfStrings(),
+                ingredientsCount = mealInfo[ColumnIndex.N_INGREDIENTS].toInt()
+            ),
+            tags = mealInfo[ColumnIndex.TAGS].toListOfStrings(),
+            nutrition = getNutrition(mealInfo[ColumnIndex.NUTRITION]),
+            steps = Steps(
+                steps = mealInfo[ColumnIndex.STEPS].toListOfStrings() ,
+                stepsCount = mealInfo[ColumnIndex.N_STEPS].toInt()
+            ),
+            submitted = mealInfo[ColumnIndex.STEPS],
+        )
     }
 
-    fun getMeals(): List<Meal> {
-        //@TODO
-        return emptyList()
-    }
+
 
     fun String.toListOfStrings(): List<String> {
         return this.trim() // Remove outer spaces
@@ -25,9 +42,21 @@ class MealsCsvParser() {
             .filter { it.isNotEmpty() }
     }
 
-    fun String.toListOfDoubles(): List<Double> {
-        //TODO
-        return emptyList()
+    fun List<String>.toListOfDoubles(): List<Double> {
+        return this.map {number-> number.toDouble() }
+    }
+
+    fun getNutrition(nutritionText:String):Nutrition{
+        val nutritionList = nutritionText.split(",").toListOfDoubles()
+        return Nutrition(
+            calories = nutritionList[0],
+            totalFat = nutritionList[1],
+            sugar = nutritionList[2],
+            sodium = nutritionList[3],
+            saturatedFat = nutritionList[4],
+            carbohydrates = nutritionList[5],
+            protein = nutritionList[6]
+        )
     }
 
 }
