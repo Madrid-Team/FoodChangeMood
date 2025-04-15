fc68ec38739431143ccb5c85679d90a3ae9b5568
Hager Abobakr <hagarabobakr1@gmail.com>
2025-04-15 13:30:54 +0200

    create project model

---
+++ src/main/kotlin/data/models/Meal.kt
@@ -0,0 +1,12 @@
+package data.models
+
+data class Meal (
+    val name:String,
+    val id:Long,
+    val minutes:Int,
+    val tags:List<String>,
+    val description:String?,
+    val nutrition:Nutrition,
+    val steps:Steps,
+    val ingredients:Ingredients
+)
