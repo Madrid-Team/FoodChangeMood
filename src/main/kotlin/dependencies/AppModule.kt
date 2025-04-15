74596b20237ce7cd2e9c7fa4ee6e98512722a086
Israa Mohamed <e.mohamed@shabek.com>
2025-04-15 20:05:00 +0200

    add current classes in app module and use case module

--- src/main/kotlin/dependencies/AppModule.kt
+++ src/main/kotlin/dependencies/AppModule.kt
@@ -1,7 +1,24 @@
 package dependencies
 
+import data.csvHandler.MealsCsvParser
+import data.csvHandler.MealsCsvReader
+import data.csvHandler.Repository.MealsRepositoryImpl
+import logic.Repository.MealsRepository
 import org.koin.dsl.module
+import java.io.File
 
 val appModule = module {
 
+    single {
+        File("food.csv")
+    }
+    single {
+        MealsCsvReader(get())
+    }
+    single {
+        MealsCsvParser()
+    }
+    single <MealsRepository>{
+        MealsRepositoryImpl(get(),get())
+    }
 }
