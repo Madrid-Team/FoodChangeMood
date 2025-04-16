7e371a3b794fb93b91c91093736ab034695b9eb2
islam elhady <i.elhady44@gmail.com>
2025-04-16 23:25:09 +0200

    add di for Guess game use case

--- src/main/kotlin/dependencies/UseCasesModule.kt
+++ src/main/kotlin/dependencies/UseCasesModule.kt
@@ -1,6 +1,7 @@
 package dependencies
 
 import logic.usecase.GetAllMealsUseCase
+import logic.usecase.StartGuessGameUseCase
 import org.koin.dsl.module
 
 
@@ -9,5 +10,6 @@ val useCaseModule = module {
     single {
         GetAllMealsUseCase()
     }
+    single { StartGuessGameUseCase(get()) }
 
 }
