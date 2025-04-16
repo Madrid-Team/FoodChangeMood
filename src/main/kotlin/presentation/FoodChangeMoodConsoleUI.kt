82b333736261333ec4c5ae40f533e29d80611feb
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-04-16 17:16:56 +0200

    add present features function in main

--- src/main/kotlin/presentation/FoodChangeMoodConsoleUI.kt
+++ src/main/kotlin/presentation/FoodChangeMoodConsoleUI.kt
@@ -18,7 +18,21 @@ class FoodChangeMoodConsoleUI(
         showOptions()
         val input = getUserInput()
         when (input) {
-
+            1 -> TODO()
+            2 -> TODO()
+            3 -> TODO()
+            4 -> TODO()
+            5 -> TODO()
+            6 -> TODO()
+            7 -> TODO()
+            8 -> TODO()
+            9 -> TODO()
+            10 -> TODO()
+            11 -> TODO()
+            12 -> TODO()
+            13 -> TODO()
+            14 -> TODO()
+            else -> println("Invalid Input .. add another input between 1 to 14 please")
         }
         presentFeatures()
     }
