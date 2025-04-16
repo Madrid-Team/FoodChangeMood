48b3221389f41c516b6e76a3d8a1177b07abf81d
Israa Mohamed <e.mohamed@shabek.com>
2025-04-16 15:47:05 +0200

    update read csv file mechanism

--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -1,17 +1,22 @@
 package presentation
 
+import data.csvHandler.ColumnIndex
+import data.csvHandler.MealsCsvParser
 import dependencies.appModule
 import dependencies.useCaseModule
+import org.apache.commons.csv.CSVParser
 import org.koin.core.context.startKoin
+import java.io.File
+import java.io.FileReader
 
 fun main() {
-
-
-
 
 
     startKoin {
         modules(appModule, useCaseModule)
     }
 
+
 }
+
+
