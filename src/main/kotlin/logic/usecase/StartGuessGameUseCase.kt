69c05b7bc1600789300fa286e9c976c66426798d
islam elhady <i.elhady44@gmail.com>
2025-04-16 23:40:51 +0200

    add start guess game with fake meal

--- src/main/kotlin/logic/usecase/StartGuessGameUseCase.kt
+++ src/main/kotlin/logic/usecase/StartGuessGameUseCase.kt
@@ -3,5 +3,26 @@ package logic.usecase
 import logic.Repository.MealsRepository
 
 class StartGuessGameUseCase(private val mealsRepository: MealsRepository) {
-    fun startGuessGame(){}
+
+    fun startGuessGame(guess: () -> Int) {
+//        val meal = mealsRepository.getAllMeals().random()
+        println("Guess the preparation time for: elhady's favourite meals ")
+        println("You have 3 attempts")
+
+        val correctTime = 20
+
+        (1..3).forEach { attempt ->
+            println("Attempt $attempt: Try to guess in minutes: ")
+            when {
+                guess() == correctTime -> {
+                    println("Correct $correctTime m")
+                    return
+                }
+
+                guess() < correctTime -> println("Too low")
+                else -> println("Too high")
+            }
+        }
+        println("The correct preparation time for elhady's favourite meals is 50 m.")
+    }
 }
--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -2,7 +2,9 @@ package presentation
 
 import dependencies.appModule
 import dependencies.useCaseModule
+import logic.usecase.StartGuessGameUseCase
 import org.koin.core.context.startKoin
+import org.koin.mp.KoinPlatform.getKoin
 
 fun main() {
 
@@ -13,5 +15,10 @@ fun main() {
     startKoin {
         modules(appModule, useCaseModule)
     }
+    val game: StartGuessGameUseCase = getKoin().get()
+
+        game.startGuessGame{ readlnOrNull()?.toIntOrNull() ?: 0}
+
+
 
 }
