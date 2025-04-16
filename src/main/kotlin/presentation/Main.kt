082198657aa450c9ac45175b9898dfa9dada4eae
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-04-16 17:04:16 +0200

    add options for user in main screen

--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -2,16 +2,14 @@ package presentation
 
 import dependencies.appModule
 import dependencies.useCaseModule
+import logic.usecase.GetAllMealsUseCase
 import org.koin.core.context.startKoin
 
 fun main() {
-
-
-
-
-
     startKoin {
         modules(appModule, useCaseModule)
     }
 
+    val consoleUI = FoodChangeMoodConsoleUI(getAllMealsUseCase = GetAllMealsUseCase())
+    consoleUI.start()
 }
