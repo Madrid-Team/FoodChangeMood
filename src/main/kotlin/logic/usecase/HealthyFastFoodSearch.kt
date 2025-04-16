033a66ccf55cd683a43af91173e0174c14016014
Mohamed Ashraf <ma1261327@gmail.com>
2025-04-16 12:50:11 +0200

    Improve HealthyFastFoodSearch class - Refactored the getSearchMeals method to understand better - Throw NotFoundHealthyFastFoodException if no meals match filters

--- src/main/kotlin/logic/usecase/HealthyFastFoodSearch.kt
+++ src/main/kotlin/logic/usecase/HealthyFastFoodSearch.kt
@@ -6,17 +6,27 @@ import logic.Repository.MealsSearch
 class HealthyFastFoodSearch : MealsSearch {
 
     override fun getSearchMeals(meal: List<Meal>): List<Meal> {
+        val mealsFiltered = meal.filter(::onlyHealthyMeal)
 
-        return meal.filter { meals ->
-            meals.minutes <= MAX_PREPARED_TIME &&
-            meals.nutrition.totalFat < MAX_TOTAL_FAT &&
-            meals.nutrition.saturatedFat < MAX_SATURATED_FAT &&
-            meals.nutrition.carbohydrates < MAX_CARBOHYDRATES
-        }.sortedBy { meals ->
-            meals.nutrition.totalFat + meals.nutrition.saturatedFat + meals.nutrition.carbohydrates
-        }.take(20)
+        if (mealsFiltered.isEmpty()) {
+            throw NotFoundHealthyFastFoodException("Not Found Meals Healthy Fast Food")
+        }
+
+        return mealsFiltered
+            .sortedBy(::getAverageNutrientsForMeal)
+            .take(TOP_HEALTHY_MEAL)
     }
 
+
+    private fun onlyHealthyMeal(meal: Meal) : Boolean {
+        return meal.minutes <= MAX_PREPARED_TIME &&
+                meal.nutrition.totalFat < MAX_TOTAL_FAT &&
+                meal.nutrition.saturatedFat < MAX_SATURATED_FAT &&
+                meal.nutrition.carbohydrates < MAX_CARBOHYDRATES
+    }
+
+
+    private fun getAverageNutrientsForMeal(meal: Meal) = meal.nutrition.totalFat + meal.nutrition.saturatedFat + meal.nutrition.carbohydrates
 
 
     companion object {
@@ -24,5 +34,8 @@ class HealthyFastFoodSearch : MealsSearch {
         const val MAX_TOTAL_FAT = 20.0
         const val MAX_SATURATED_FAT = 5.0
         const val MAX_CARBOHYDRATES = 50.0
+        const val TOP_HEALTHY_MEAL = 15
     }
 }
+
+class NotFoundHealthyFastFoodException(message: String) : Exception(message)
