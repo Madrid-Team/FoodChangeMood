0e044b88c7c638b2c1336d6c5fbd66e53fdd54b0
islam elhady <i.elhady44@gmail.com>
2025-04-17 06:40:58 +0200

    refactor start guess game logic in use case

--- src/main/kotlin/logic/usecase/StartGuessGameUseCase.kt
+++ src/main/kotlin/logic/usecase/StartGuessGameUseCase.kt
@@ -4,25 +4,17 @@ import logic.Repository.MealsRepository
 
 class StartGuessGameUseCase(private val mealsRepository: MealsRepository) {
 
-    fun startGuessGame(guess: () -> Int) {
-//        val meal = mealsRepository.getAllMeals().random()
-        println("Guess the preparation time for: elhady's favourite meals ")
-        println("You have 3 attempts")
-
-        val correctTime = 20
-
-        (1..3).forEach { attempt ->
-            println("Attempt $attempt: Try to guess in minutes: ")
-            when {
-                guess() == correctTime -> {
-                    println("Correct $correctTime m")
-                    return
-                }
-
-                guess() < correctTime -> println("Too low")
-                else -> println("Too high")
-            }
+    fun startGuessGame(guess: Int, correctTime: Int): String {
+        return when {
+            guess == correctTime -> "Correct"
+            guess < correctTime -> "Too low"
+            else -> "Too high"
         }
-        println("The correct preparation time for elhady's favourite meals is 50 m.")
+    }
+
+    fun getRandomMeal(): Pair<String, Int> {
+        return mealsRepository.getAllMeals()
+            .random()
+            .let { it.name to it.minutes }
     }
 }
