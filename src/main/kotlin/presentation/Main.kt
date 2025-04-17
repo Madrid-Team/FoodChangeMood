c30d588c5866ba21919c22e018d43cde48d823bc
islam elhady <i.elhady44@gmail.com>
2025-04-17 06:44:46 +0200

    clean up main by removing game logic call

--- src/main/kotlin/presentation/Main.kt
+++ src/main/kotlin/presentation/Main.kt
@@ -2,9 +2,7 @@ package presentation
 
 import dependencies.appModule
 import dependencies.useCaseModule
-import logic.usecase.StartGuessGameUseCase
 import org.koin.core.context.startKoin
-import org.koin.mp.KoinPlatform.getKoin
 
 fun main() {
 
@@ -15,9 +13,6 @@ fun main() {
     startKoin {
         modules(appModule, useCaseModule)
     }
-    val game: StartGuessGameUseCase = getKoin().get()
-
-        game.startGuessGame{ readlnOrNull()?.toIntOrNull() ?: 0}
 
 
 
