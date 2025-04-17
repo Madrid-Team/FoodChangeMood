525c0129f8da536d9fd98044c6df76284edeb82e
IslamMagd <eslam752001.magdy54@gmail.com>
2025-04-17 18:03:15 +0200

    make sweet free eggs list cashed to avoid filter it evey call

--- src/main/kotlin/logic/usecase/GetSweetsWithNoEggsUseCase.kt
+++ src/main/kotlin/logic/usecase/GetSweetsWithNoEggsUseCase.kt
@@ -7,13 +7,22 @@ class GetSweetsWithNoEggsUseCase(private val mealsRepository: MealsRepository) {
 
     fun getfSweetFreeEggs(): Meal {
         val sweetFreeEggsId = mutableListOf<Int>()
+        var sweetFreeEggsCash = emptyList<Meal>()
+        var isFirstCall = true
 
-        val sweetFreeEgss = mealsRepository.getAllMeals().filter { meal ->
-            meal.tags.contains("sweet") && !meal.ingredients.ingredients.contains("egg") && meal.id !in sweetFreeEggsId
-        }.random()
-        sweetFreeEgss.let { sweet -> sweetFreeEggsId.add(sweet.id) }
+        if (isFirstCall) {
+            sweetFreeEggsCash = mealsRepository.getAllMeals().filter { meal ->
+                meal.tags.contains("sweet") && !meal.ingredients.ingredients.contains("egg")
+            }
+            isFirstCall = false
+        }
 
-        return sweetFreeEgss
+        val remainningSweets = sweetFreeEggsCash.filter { it.id !in sweetFreeEggsId }
+
+        val nextMeal = remainningSweets.random()
+        nextMeal.let { sweetFreeEggsId.add(nextMeal.id) }
+
+        return nextMeal
     }
 
 }
