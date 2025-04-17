15fb84e9fd2e28f242b7551b1087cfccee614783
IslamMagd <eslam752001.magdy54@gmail.com>
2025-04-17 16:42:50 +0200

    implement basic method to get random sweet free eggs

--- src/main/kotlin/logic/usecase/GetSweetsWithNoEggsUseCase.kt
+++ src/main/kotlin/logic/usecase/GetSweetsWithNoEggsUseCase.kt
@@ -1,7 +1,14 @@
 package logic.usecase
 
+import data.models.Meal
 import logic.Repository.MealsRepository
 
 class GetSweetsWithNoEggsUseCase(private val mealsRepository: MealsRepository) {
+
+    fun getfSweetFreeEggs(): Meal {
+        return mealsRepository.getAllMeals().filter { meal ->
+            meal.tags.contains("sweet") && !meal.ingredients.ingredients.contains("egg")
+        }.random()
+    }
 
 }
