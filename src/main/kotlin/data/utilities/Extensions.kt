2631312c2ca0ac55fd36dc8668e63276e685c59c
khater <khater@email.com>
2025-04-18 19:00:31 +0200

    add companion properties for hard coded chars and extension functions for readability

--- src/main/kotlin/data/utilities/Extensions.kt
+++ src/main/kotlin/data/utilities/Extensions.kt
@@ -4,3 +4,32 @@ fun splitIgnoringQuotedCommas(input: String): List<String> {
     val pattern = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"
     return input.split(pattern.toRegex())
 }
+
+
+
+val String.Companion.fileNotFound:String get() = "File not found"
+
+val String.Companion.empty: String get() = ""
+val Char.Companion.doubleQuotes: Char get() = '"'
+val String.Companion.singleQuote: String get() = "'"
+val String.Companion.comma: String get() = ","
+val String.Companion.openBracket: String get() = "["
+val String.Companion.closeBracket: String get() = "]"
+
+fun List<String>.dropHeader(): List<String> = this.drop(1)
+
+fun String.appendLine(line: String): String = if (this.isEmpty()) line else this + line
+
+fun String.removeSurroundingQuotes(): String = this.removeSurrounding(Char.doubleQuotes.toString())
+fun String.removeBrackets(): String = this.removePrefix(String.openBracket).removeSuffix(String.closeBracket)
+fun List<String>.removeSingleQuotesFromElements(): List<String> =
+    this.map { it.replace(String.singleQuote, String.empty) }
+fun String.splitByComma(): List<String> = this.split(String.comma)
+
+fun List<String>.trimElements(): List<String> = this.map { it.trim() }
+fun List<String>.removeEmptyElements(): List<String> = this.filter { it.isNotEmpty() }
+
+val Int.isEven: Boolean
+    get() = this % 2 == 0
+
+
