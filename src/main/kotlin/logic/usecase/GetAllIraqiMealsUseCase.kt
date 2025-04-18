10595e028006a264fc67f0379a97470529d6b5f4
Hager Abobakr <hagarabobakr1@gmail.com>
2025-04-18 06:05:37 +0200

    add companion object in get all iraqi meals use case

--- src/main/kotlin/logic/usecase/GetAllIraqiMealsUseCase.kt
+++ src/main/kotlin/logic/usecase/GetAllIraqiMealsUseCase.kt
@@ -4,10 +4,18 @@ import data.models.Meal
 import logic.Repository.MealsRepository
 
 class GetAllIraqiMealsUseCase(private val repository: MealsRepository) {
-    fun getAllIraqiMeals():List<Meal>{
+    fun getAllIraqiMeals(): List<Meal> {
         return repository.getAllMeals()
-            .filter { it.tags.any { it.contains("iraqi", ignoreCase = true) }
-                    ||(it.description?.contains("iraq", ignoreCase = true)?:false ) }
+            .filter {
+                it.tags.any { it.contains(IRAQI, ignoreCase = true) }
+                        || (it.description?.contains(IRAQ, ignoreCase = true) ?: false)
+            }
+    }
+
+    companion object {
+        const val IRAQI = "iraqi"
+        const val IRAQ = "iraq"
+
     }
 }
 
