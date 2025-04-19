cd5736d162445c1150c0eb33d50edb38b6ffc635
Mahmmed Naser <mahmmedn19@gmail.com>
2025-04-19 12:21:07 +0200

    refactor: remove unused use case imports from UseCasesModule.kt

--- src/main/kotlin/dependencies/UseCasesModule.kt
+++ src/main/kotlin/dependencies/UseCasesModule.kt
@@ -1,15 +1,10 @@
 package dependencies
 
-import logic.usecase.GetAllMealsUseCase
-import logic.usecase.MealSearchingByNameUseCase
 import logic.usecase.*
 import logic.usecase.mealIngredientsGame.GetGameScoreUseCase
 import logic.usecase.mealIngredientsGame.GetIngredientGameRandomMealUseCase
 import logic.usecase.mealIngredientsGame.GetNIncorrectIngredientsUseCase
 import logic.usecase.mealIngredientsGame.MakeGuessUseCase
-import logic.usecase.SuggestMealWithHighCalorieUseCase
-import logic.usecase.GetSweetsWithNoEggsUseCase
-import logic.usecase.GetMealsSuitableForGymUseCase
 import org.koin.dsl.module
 import presentation.GuessGameConsoleUi
 
