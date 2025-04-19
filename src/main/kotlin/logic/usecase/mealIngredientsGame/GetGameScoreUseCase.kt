684beef6a57f37d2de4bbfec11447da6eecb61fb
khater <khater@email.com>
2025-04-19 20:36:34 +0200

    fix hard coded strings and magic numbers in all files

--- src/main/kotlin/logic/usecase/mealIngredientsGame/GetGameScoreUseCase.kt
+++ src/main/kotlin/logic/usecase/mealIngredientsGame/GetGameScoreUseCase.kt
@@ -1,11 +1,12 @@
 package logic.usecase.mealIngredientsGame
 
+import data.csvHandler.Tags
 import logic.Repository.MealsRepository
 
 class GetGameScoreUseCase(private val repository: MealsRepository) {
 
     operator fun invoke(): Int {
-        return repository.getCorrectGuessedMealsNames().size * 1000
+        return repository.getCorrectGuessedMealsNames().size * Tags.GameScore.CORRECT_GUESS_POINTS
     }
 
 }
