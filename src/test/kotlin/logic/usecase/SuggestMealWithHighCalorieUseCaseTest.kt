462e4f2a68ad5d9c02d36f6c4ca7e375976ddca5
islam elhady <i.elhady44@gmail.com>
2025-04-23 08:15:16 +0200

    test: add test for valid high calorie meal

--- src/test/kotlin/logic/usecase/SuggestMealWithHighCalorieUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/SuggestMealWithHighCalorieUseCaseTest.kt
@@ -1,8 +1,16 @@
 package logic.usecase
 
+import com.google.common.truth.Truth.assertThat
+import data.models.Ingredients
+import data.models.Meal
+import data.models.Nutrition
+import data.models.Steps
+import io.mockk.every
 import io.mockk.mockk
 import logic.Repository.MealsRepository
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.Date
 
 class SuggestMealWithHighCalorieUseCaseTest {
 
@@ -13,5 +21,55 @@ class SuggestMealWithHighCalorieUseCaseTest {
     fun setup() {
         mealsRepository = mockk(relaxed = true)
         useCase = SuggestMealWithHighCalorieUseCase(mealsRepository)
+    }
+
+    @Test
+    fun `should return high calorie meal when is available`(){
+        val meals = listOf(
+            Meal(
+                name = "",
+                id = 1,
+                minutes = 1,
+                tags = listOf(),
+                description = "delicious toast",
+                nutrition = Nutrition(450.0, 0.0, 0.0, 0.0, 0.0, 0.0, 50.0),
+                steps = Steps(listOf(),0),
+                ingredients = Ingredients(listOf(),0),
+                submitted = Date(),
+                contributorId = 101
+            ),
+            Meal(
+                name = "",
+                id = 2,
+                minutes = 10,
+                tags = listOf(),
+                description = "delicious toast",
+                nutrition = Nutrition(250.0, 0.0, 0.0, 0.0, 0.0, 0.0, 50.0),
+                steps = Steps(listOf(),0),
+                ingredients = Ingredients(listOf(),0),
+                submitted = Date(),
+                contributorId = 102
+            ),
+            Meal(
+                name = "",
+                id = 3,
+                minutes = 20,
+                tags = listOf("", "", ""),
+                description = "delicious toast",
+                nutrition = Nutrition(850.0, 0.0, 0.0, 0.0, 0.0, 0.0, 50.0),
+                steps = Steps(listOf(),0),
+                ingredients = Ingredients(listOf(),0),
+                submitted = Date(),
+                contributorId = 103
+            )
+        )
+
+        every { mealsRepository.getAllMeals() } returns meals
+
+        val result = useCase.suggestRandomHighCalorieMeal(setOf())
+
+        assertThat(result!!.nutrition.calories).isGreaterThan(700.0)
+
+
     }
 }
