c03918b920b5b6be4e3c3276c4a5bd67fc0cc3fd
Hager Abobakr <hagarabobakr1@gmail.com>
2025-04-23 20:48:23 +0200

    Add initial test setup for filtering Iraqi meals by tags or description

--- src/test/kotlin/logic/usecase/GetAllIraqiMealsUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/GetAllIraqiMealsUseCaseTest.kt
@@ -1,8 +1,16 @@
 package logic.usecase
 
+import com.google.common.truth.Truth
+import data.models.Ingredients
+import data.models.Meal
+import data.models.Nutrition
+import data.models.Steps
+import io.mockk.every
 import io.mockk.mockk
 import logic.Repository.MealsRepository
 import org.junit.jupiter.api.BeforeEach
+import java.util.*
+import kotlin.test.Test
 
 class GetAllIraqiMealsUseCaseTest() {
     private lateinit var repository: MealsRepository
@@ -14,5 +22,15 @@ class GetAllIraqiMealsUseCaseTest() {
         getAllIraqiMealsUseCase = GetAllIraqiMealsUseCase(repository)
     }
 
+
+    @Test
+    fun `should return all meals containing iraqi in tags or description`() {
+        //Given
+
+        // When
+
+        //Then
+
+    }
 
 }
