bdff2dc8ee4ba03f63d732b8d5a37292034e7203
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-04-23 19:26:41 +0200

    add three test cases for search meal by name feature

--- src/test/kotlin/test/MealSearchingByNameUseCaseTest.kt
+++ src/test/kotlin/test/MealSearchingByNameUseCaseTest.kt
@@ -7,6 +7,9 @@ import logic.Repository.MealsRepository
 import logic.usecase.MealSearchingByNameUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class MealSearchingByNameUseCaseTest {
     private lateinit var mealsRepository: MealsRepository
@@ -29,6 +32,53 @@ class MealSearchingByNameUseCaseTest {
 
         //when
         val result = mealSearchingByNameUseCase.searchAboutMealByName("pasta")
+
+        //then
+        assertThat(result.map { it.name }).contains("pasta")
+    }
+
+    @Test
+    fun `should return throw exception when search about not found meals`() {
+        //given
+        every { mealsRepository.getAllMeals() } returns listOf(
+            createMeals("pasta"),
+            createMeals("rice"),
+            createMeals("meat")
+        )
+
+        //when && then
+        assertThrows<NoSuchElementException> {
+            mealSearchingByNameUseCase.searchAboutMealByName("koshary")
+        }
+    }
+
+    @Test
+    fun `should return throw exception when search about empty input`() {
+        //given
+        every { mealsRepository.getAllMeals() } returns listOf(
+            createMeals("pasta"),
+            createMeals("rice"),
+            createMeals("meat")
+        )
+
+        //when && then
+        assertThrows<NoSuchElementException> {
+            mealSearchingByNameUseCase.searchAboutMealByName(" ")
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["pasta", "PASTA", "Pasta", "PasTa"])
+    fun `should return meals when searched about meal with upper and lower cases`(searchInput: String) {
+        //given
+        every { mealsRepository.getAllMeals() } returns listOf(
+            createMeals("pasta"),
+            createMeals("rice"),
+            createMeals("meat")
+        )
+
+        //when
+        val result = mealSearchingByNameUseCase.searchAboutMealByName(searchInput)
 
         //then
         assertThat(result.map { it.name }).contains("pasta")
