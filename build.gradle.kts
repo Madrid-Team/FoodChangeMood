048b3154b55a698e50a7d659d33d3bd57ba66160
Israa Mohamed <e.mohamed@shabek.com>
2025-04-23 23:33:35 +0200

    add test cases for MealsCsvReader class

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+
     implementation("io.insert-koin:koin-core:3.5.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.0-M2")
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("com.google.truth:truth:1.4.2")
+    testImplementation("io.mockk:mockk:1.14.0")
 }
 
 tasks.test {
--- src/main/kotlin/data/csvHandler/MealsCsvReader.kt
+++ src/main/kotlin/data/csvHandler/MealsCsvReader.kt
@@ -3,6 +3,7 @@ package data.csvHandler
 import data.utilities.*
 import java.io.File
 import java.io.FileNotFoundException
+import java.security.cert.Extension
 
 class MealsCsvReader(
     private val file: File
@@ -10,25 +11,30 @@ class MealsCsvReader(
 
     fun readCsvFile(): List<String> {
         if (file.exists()) {
-
-            val rows = mutableListOf<String>()
-            var currentRow = String.empty
-
-            for (line in file.readLines().dropHeader()) {
-                currentRow = currentRow.appendLine(line)
-
-                val quoteCount = currentRow.count { it == Char.doubleQuotes }
-                if (quoteCount.isEven) {
-                    rows.add(currentRow)
-                    currentRow = String.empty
-                }
-            }
-
-            return rows
+            return getAllRows()
         } else
             throw FileNotFoundException(String.fileNotFound)
     }
 
+    private fun getAllRows(): List<String> {
+        val rows = mutableListOf<String>()
+        var currentRow = String.empty
+
+        for (line in file.readLines().dropHeader()) {
+            currentRow = currentRow.appendLine(line)
+
+            val quoteCount = currentRow.count { it == Char.doubleQuotes }
+            if (quoteCount.isEven) {
+                rows.add(currentRow)
+                currentRow = String.empty
+            }
+        }
+
+        return if (rows.isEmpty())
+            throw MealsExceptions.MealNotFoundException("No meals found")
+        else
+            rows
+    }
 
 
 }
