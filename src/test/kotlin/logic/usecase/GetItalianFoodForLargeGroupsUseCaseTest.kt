0b6e57859ddac0afb29e5c38316857e31b72c293
abdelrahman <abdelrahmankarim7@gmail.com>
2025-04-24 16:35:46 +0200

    add:italian food for large groups test class

---
+++ src/test/kotlin/logic/usecase/GetItalianFoodForLargeGroupsUseCaseTest.kt
@@ -0,0 +1,15 @@
+package logic.usecase
+
+import io.mockk.mockk
+import logic.Repository.MealsRepository
+import org.junit.jupiter.api.BeforeEach
+
+class GetItalianFoodForLargeGroupsUseCaseTest {
+    private lateinit var mealRepository: MealsRepository
+    private lateinit var getItalianFoodForLargeGroupsUseCase: GetItalianFoodForLargeGroupsUseCase
+
+
+
+
+
+}
