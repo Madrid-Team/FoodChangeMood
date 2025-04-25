85b2b1db2b661f8d89e6aa0616dff59b00610ed0
abdelrahman <abdelrahmankarim7@gmail.com>
2025-04-25 07:58:55 +0200

    add : Test for valid italian for large groups meals

--- src/test/kotlin/logic/usecase/GetItalianFoodForLargeGroupsUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/GetItalianFoodForLargeGroupsUseCaseTest.kt
@@ -1,5 +1,6 @@
 package logic.usecase
 
+import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
 import logic.Repository.MealsRepository
@@ -21,19 +22,33 @@ class GetItalianFoodForLargeGroupsUseCaseTest {
     fun `should return valid meals when the list has italian food for large groups`(){
         //Given
         every { mealRepository.getAllMeals() } returns listOf(
-            createMealItalianLargeGroups("pasta" ,2 , tags = listOf())
+            createMealItalianLargeGroups("pasta" , 1 ,
+                tags = listOf("spicy" , "for-large-groups") ,
+                description = "ITALIAN food can be made in 10 minutes" ),
 
+            createMealItalianLargeGroups("koshari" , 2 ,
+                tags = listOf("spicy" , "for-large-groups") ,
+                description = "egyptian food can be made in 10 minutes" ),
 
-
+            createMealItalianLargeGroups("pistatchini" , 3 ,
+                tags = listOf("salsa" , "for-small-groups") ,
+                description = "italian food can be made in 10 minutes" ),
+            createMealItalianLargeGroups("pizza" , 4 ,
+                tags = listOf("spicy" , "FOR-LARGE-Groups") ,
+                description = "italian food can be made in 10 minutes" )
 
         )
-
-
         //when
-
-
+        val resultMeals = getItalianFoodForLargeGroupsUseCase.getItalianFoodForLargeGroups()
 
         //then
+        assertThat(resultMeals).hasSize(2)
+
+
+    }
+
+    @Test
+    fun `should return throw exception when list is empty`(){
 
 
 
