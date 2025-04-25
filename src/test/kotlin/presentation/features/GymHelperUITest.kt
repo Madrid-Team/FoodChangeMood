c6cb646d48c36140167d7cede9c5c1f515886765
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-04-25 23:18:01 +0300

    add ui test for gym helper ui feature

--- src/test/kotlin/presentation/features/GymHelperUITest.kt
+++ src/test/kotlin/presentation/features/GymHelperUITest.kt
@@ -1,5 +1,6 @@
 package presentation.features
 
+import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -25,36 +26,87 @@ class GymHelperUITest {
     }
 
     @Test
-    fun `should show Invalid input when calories is null`() {
-        every { reader.readDouble() } returns null
+    fun `ui class should have the correct properties value`() {
+        val classId = 9
+        val uiMessage = "$classId- Enter a desired amount of calories and protein,\n" +
+                "and return a list of meals that match or approximate those values."
 
+        assertThat(gymHelperUI.id).isEqualTo(classId)
+        assertThat(gymHelperUI.message).isEqualTo(uiMessage)
+    }
+
+    @Test
+    fun `ui should return meals when input is valid`() {
+        //given
+        every { reader.readDouble() } returns validCalories andThen validProtein
+        every {
+            getMealsSuitableForGymUseCase.getMealsWithinCalorieAndProteinRange(
+                validCalories,
+                validProtein
+            )
+        } returns meals
+
+        //when
         gymHelperUI.start()
 
-        verify {
-            viewer.show("Invalid input")
+        //then
+        meals.forEach {
+            viewer.show(it.name)
         }
     }
 
     @Test
-    fun `should show NoSuchElementException message when list is empty `() {
-        val calories = 5.5
-        val protein = 7.8
-        every { reader.readDouble() } returns calories andThen protein
-        every { getMealsSuitableForGymUseCase.getMealsWithinCalorieAndProteinRange(calories, protein) } returns listOf(
+    fun `ui should show NoSuchElementException message when calories value not valid`() {
+        every { reader.readDouble() } returns notValidCalories andThen validProtein
+        every {
+            getMealsSuitableForGymUseCase.getMealsWithinCalorieAndProteinRange(
+                notValidCalories,
+                validProtein
+            )
+        } throws NoSuchElementException()
+
+        gymHelperUI.start()
+
+        verify { viewer.show(NoSuchElementException().message.toString()) }
+    }
+
+    @Test
+    fun `ui should show NoSuchElementException message when protein value not valid`() {
+        every { reader.readDouble() } returns validCalories andThen notValidProtein
+        every {
+            getMealsSuitableForGymUseCase.getMealsWithinCalorieAndProteinRange(
+                validCalories,
+                notValidProtein
+            )
+        } throws NoSuchElementException()
+
+        gymHelperUI.start()
+
+        verify { viewer.show(NoSuchElementException().message.toString()) }
+    }
+
+    @Test
+    fun `ui should show message when protein or protein value equal null`() {
+        every { reader.readDouble() } returns null andThen null
+
+        gymHelperUI.start()
+
+        verify { viewer.show("Invalid input") }
+    }
+
+
+    companion object {
+        private const val validCalories = 5.5
+        private const val validProtein = 7.8
+
+        private const val notValidCalories = 512.5
+        private const val notValidProtein = 524.5
+
+        private val meals = listOf(
             createMeal(320.0, 25.0),
             createMeal(38.0, 92.0),
             createMeal(60.0, 30.0),
             createMeal(150.0, 210.0)
         )
-
-        gymHelperUI.start()
-
-        verify {
-            viewer.show("No meals found matching calories and proten even withn range 10")
-        }
     }
-
-
-
-
 }
