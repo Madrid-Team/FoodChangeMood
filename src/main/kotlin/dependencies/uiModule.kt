04232fdebd06d9b2fefc9c06e0abcc43ea6ca920
islam elhady <i.elhady44@gmail.com>
2025-04-25 15:35:22 +0300

    inject Viewer into GuessGameConsoleUi for output handling

--- src/main/kotlin/dependencies/uiModule.kt
+++ src/main/kotlin/dependencies/uiModule.kt
@@ -10,7 +10,7 @@ val uiModule = module {
     single<Reader> { ConsoleReader() }
     single<Viewer> { ConsoleViewer() }
 
-    single { GuessGameConsoleUi(get()) } bind BaseUIController::class
+    single { GuessGameConsoleUi(get(), get()) } bind BaseUIController::class
     single { MealIngredientsGameUI(get(), get(), get()) } bind BaseUIController::class
     single { SearchMealsByDateUI(get()) } bind BaseUIController::class
     single { SuggestEasyMealGameUI(get()) } bind BaseUIController::class
