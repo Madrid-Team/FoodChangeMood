0be458c44d74d80f3fdbe67cf07b2671febdc4f6
IslamMagd <eslam752001.magdy54@gmail.com>
2025-04-26 00:06:06 +0300

    add test cases to LargeItaliansMealsUITest.kt

--- src/test/kotlin/presentation/features/LargeItaliansMealsUITest.kt
+++ src/test/kotlin/presentation/features/LargeItaliansMealsUITest.kt
@@ -1,15 +1,12 @@
 package presentation.features
 
+import createMeal
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import logic.usecase.GetItalianFoodForLargeGroupsUseCase
-import logic.usecase.GetMealsSuitableForGymUseCase
-import logic.usecase.createMeal
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import presentation.common.ConsoleReader
 import presentation.common.ConsoleViewer
 
 class LargeItaliansMealsUITest {
@@ -25,16 +22,43 @@ class LargeItaliansMealsUITest {
     }
 
     @Test
-    fun `should show NoSuchElementException message when list is empty `() {
+    fun `should show NoSuchElementException message when list is empty`() {
         every { getMealsSuitableForGymUseCase.getItalianFoodForLargeGroups() } returns emptyList()
 
         largeItaliansMealsUI.start()
 
         verify {
-            viewer.show("no element found")
+            viewer.show("No italian food found for large groups")
         }
     }
 
+    @Test
+    fun `should show each meal name when list is not empty`() {
+        every { getMealsSuitableForGymUseCase.getItalianFoodForLargeGroups() } returns listOf(
+            createMeal(
+                name = "pasta",
+                tags = listOf("spicy", "for-large-groups"),
+                description = "ITALIAN food can be made in 10 minutes"
+            ),
+            createMeal(
+                name = "koshari",
+                tags = listOf("spicy", "for-large-groups"),
+                description = "egyptian food can be made in 10 minutes"
+            ),
+            createMeal(
+                name = "pistatchini",
+                tags = listOf("salsa", "for-small-groups"),
+                description = "italian food can be made in 10 minutes"
+            )
+        )
+
+        largeItaliansMealsUI.start()
+
+        verify {
+            viewer.show("pasta")
+            viewer.show("pistatchini")
+        }
+    }
 
 }
 
